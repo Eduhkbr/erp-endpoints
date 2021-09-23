57d39d454b0f45ff746ba29cb95f945da9d2cf89
eduhkbr <edu.abreu09@hotmail.com>
2021-09-22 23:06:45 -0300

    Application inicial

---
+++ erp/src/main/java/br/com/abreu/erp/controller/PessoaController.java
@@ -0,0 +1,18 @@
+package br.com.abreu.erp.controller;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/pessoas")
+public class PessoaController {
+
+    @GetMapping("")
+    public ResponseEntity<String> consultarPessoas() {
+        return ResponseEntity.ok("Hello World");
+    }
+}
